6fa5f65ec6c48bcb09679722f5107da4ab21982e
KCAU-Tech <tech@students.kcau.ac.ke>
2025-02-04 21:22:31 +0300

    Refactored PULL_REQUEST_TEMPLATE location

--- functions/index.js
+++ functions/index.js
@@ -16,6 +16,7 @@ const logger = require("firebase-functions/logger");
 exports.helloWorld = onRequest((request, response) => {
   logger.info("Hello logs!", {structuredData: true});
   response.send({
+    title: "Firebase Cloud Functions",
     message: "Hello from Firebase!",
   });
 });
